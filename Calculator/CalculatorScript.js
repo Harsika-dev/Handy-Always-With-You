a6594b3c86da54e519e0ebb8e3936527d64d86a0
Harsika Agarwal <76247860+Harsika-dev@users.noreply.github.com>
2021-12-18 22:33:04 +0530

    Add files via upload

---
+++ Calculator/CalculatorScript.js
@@ -0,0 +1,108 @@
+var screen=document.querySelector('#screen');
+    var btn=document.querySelectorAll('.btn');
+
+    /*============ For getting the value of btn, Here we use for loop ============*/
+    for(item of btn)
+    {
+        item.addEventListener('click',(e)=>{
+            btntext=e.target.innerText;
+
+            if(btntext =='×')
+            {
+                btntext= '*';
+            }
+
+            if(btntext=='÷')
+            {
+                btntext='/';
+            }
+            screen.value+=btntext;
+        });
+    }
+
+    function sin()
+    {
+        screen.value=Math.sin(screen.value);
+    }
+
+    function cos()
+    {
+        screen.value=Math.cos(screen.value);
+    }
+
+    function tan()
+    {
+        screen.value=Math.tan(screen.value);
+    }
+
+    function pow()
+    {
+        screen.value=Math.pow(screen.value,2);
+    }
+
+    function sqrt()
+    {
+        screen.value=Math.sqrt(screen.value,2);
+    }
+
+    function log()
+    {
+        screen.value=Math.log(screen.value);
+    }
+
+    function pi()
+    {
+        screen.value= 3.14159265359;
+    }
+
+    function e()
+    {
+        screen.value=2.71828182846;
+    }
+
+    function fact()
+    {
+        var i, num, f;
+        f=1
+        num=screen.value;
+        for(i=1; i<=num; i++)
+        {
+            f=f*i;
+        }
+
+        i=i-1;
+
+        screen.value=f;
+    }
+
+    function backspc()
+    {
+        screen.value=screen.value.substr(0,screen.value.length-1);
+    }
+
+
+
+
+
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  menuBtnChange();//calling the function(optional)
+});
+
+searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+ }
+}
